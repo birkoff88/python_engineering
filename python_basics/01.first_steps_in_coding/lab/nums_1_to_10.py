35c46ed0eab01047355e0eebf6ba1656da3c0bd9
Boris Petrov <boris.petrov88@abv.bg>
2026-09-03 07:17:45 +0300

    basics lab + gitignore

--- python_basics/01.first_steps_in_coding/lab/nums_1_to_10.py
+++ python_basics/01.first_steps_in_coding/lab/nums_1_to_10.py
@@ -1,10 +1,10 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
